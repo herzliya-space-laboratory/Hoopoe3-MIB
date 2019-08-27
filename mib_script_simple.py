7f86025d7f7f4b52923741f2da0e2ee2932d5c87
adiwwww <adiwwww@gmail.com>
2019-08-27 20:43:39 +0300

    Yet another change - closing the file

--- mib_script_simple.py
+++ mib_script_simple.py
@@ -9,7 +9,7 @@ Created on Tue Aug 27 19:17:39 2019
 #==============================================================================
 # Steps to activate the automotatic SST and parameters script:
 # 1. Copy the information from one Sub System (All coloums) to excel
-# 2. Save the excel file by name: "TLC.csv" as a *comma delimited csv* file
+# 2. Save the excel file by name: "TLM.csv" as a *comma delimited csv* file
 # 3. Run the program by double clicking it
 # 4. Mark the result from the cmd, and copy it by right clicking the upper bar
 #    and choosing the "Copy option"
@@ -21,6 +21,7 @@ def read_csv(file_name):
         data =[]
         for row in csv_reader:
             data.append(row)
+    csv_file.close
     return data
     
 def sst_data(sst_value,sst_name):
@@ -31,7 +32,7 @@ def parameter_data(param_name,param_type,param_unit,param_little ="false"):
     param_text = "  <Parameter Name=\""+param_name+"\" Type=\""+param_type+"\" Unit=\""+param_unit+"\" IsLittleEndian=\"" +param_little+ "\" />\n"
     return param_text
 
-source_file = "TLC.csv" ###Write here the name of the new file
+source_file = "TLM.csv" ###Write here the name of the new file
 data = read_csv(source_file)
 
 result=""
