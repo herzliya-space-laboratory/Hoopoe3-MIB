58f6bae4ea3e15704da19852a25bdc7d3ae241a8
adiwwww <adiwwww@gmail.com>
2019-08-27 20:41:00 +0300

    Making some chages in the automatic

--- mib_script_simple.py
+++ mib_script_simple.py
@@ -5,6 +5,15 @@ Created on Tue Aug 27 19:17:39 2019
 @author: User
 """
 
+
+#==============================================================================
+# Steps to activate the automotatic SST and parameters script:
+# 1. Copy the information from one Sub System (All coloums) to excel
+# 2. Save the excel file by name: "TLC.csv" as a *comma delimited csv* file
+# 3. Run the program by double clicking it
+# 4. Mark the result from the cmd, and copy it by right clicking the upper bar
+#    and choosing the "Copy option"
+#==============================================================================
 import csv
 def read_csv(file_name):
     with open(file_name) as csv_file:
@@ -22,7 +31,7 @@ def parameter_data(param_name,param_type,param_unit,param_little ="false"):
     param_text = "  <Parameter Name=\""+param_name+"\" Type=\""+param_type+"\" Unit=\""+param_unit+"\" IsLittleEndian=\"" +param_little+ "\" />\n"
     return param_text
 
-source_file = "EPS_Telemetry.csv"
+source_file = "TLC.csv" ###Write here the name of the new file
 data = read_csv(source_file)
 
 result=""
@@ -36,3 +45,6 @@ for row in range(0, len(data)):
         param_type = data[row][6]
         param_unit = data[row][7]
         result+=parameter_data(param_name,param_type,param_unit)
+result += "</ServiceSubtype>\n\n"
+print(result)
+input("Press Enter to continue...")
