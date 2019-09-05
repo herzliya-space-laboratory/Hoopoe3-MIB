92f652379c260177df5de315c6db5bd0956eb635
Adi Rabinovitz <adiwwww@gmail.com>
2019-09-05 14:16:26 +0300

    script

--- TLC_script.py
+++ TLC_script.py
@@ -29,7 +29,13 @@ def sst_data(sst_value,sst_name):
     return sst_text
 
 def parameter_data(param_name,param_type,param_unit,param_little ="false"):
-    param_text = "  <Parameter Name=\""+param_name+"\" Type=\""+param_type+"\" Unit=\""+param_unit+"\" IsLittleEndian=\"" +param_little+ "\" />\n"
+    param_text=""
+    if (param_name=="-" or param_name==""):
+        return param_text
+    if (param_unit=="" or param_unit=="-" or param_unit=="N"):
+        param_text = "  <Parameter Name=\""+param_name+"\" Type=\""+param_type+"\" IsLittleEndian=\"" +param_little+ "\" />\n"
+    else:
+        param_text = "  <Parameter Name=\""+param_name+"\" Type=\""+param_type+"\" Unit=\""+param_unit+"\" IsLittleEndian=\"" +param_little+ "\" />\n"
     return param_text
 
 source_file = "TLC.csv" ###Write here the name of the new file
@@ -37,10 +43,15 @@ data = read_csv(source_file)
 
 result=""
 for row in range(0, len(data)):
-    if(data[row][1]!=''):
+    if(data[row][3]!=''):
         if (row>0):
             result += "</ServiceSubtype>\n\n"
         result += sst_data(data[row][5],data[row][3])
+        param_name = data[row][6]
+        param_type = data[row][7]
+        param_unit = data[row][9]
+        result+=parameter_data(param_name,param_type,param_unit)
+
     else:
         param_name = data[row][6]
         param_type = data[row][7]
